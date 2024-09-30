0257dfb77379d40e9df725c05430eb55d978f789
kleiry20 <74005559+kleiry20@users.noreply.github.com>
2024-10-01 01:15:04 +0530

    donw till part 2

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    
+    # apps
+    "polls.apps.PollsConfig",
 ]
 
 MIDDLEWARE = [
@@ -105,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "IST"
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
